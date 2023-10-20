b936c47e29d119ea49f344663608810fa170089a
Geckkou <alexandre.asr2001@hotmail.com>
2023-10-20 19:11:59 -0300

    hotfix

--- GRP 25/src/router.js
+++ GRP 25/src/router.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const endpointsController = require('./controllers/endpointsController');
 
-
-router.get('/test', endpointsController.getAll);
+router.get('/Fimdia', endpointsController.truncateDia);
 router.get('/SenhaSP', endpointsController.insertSenhaSP); 
 router.get('/SenhaSE', endpointsController.insertSenhaSE);
 router.get('/SenhaSG', endpointsController.insertSenhaSG); 
@@ -12,5 +11,3 @@ router.get('/Display', endpointsController.displayTemp);
 router.get('/Proximo',endpointsController.proximo);
 router.get('/relatorio', endpointsController.relatorio);
 module.exports = router;
-
-
